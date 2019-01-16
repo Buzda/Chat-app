0f731eff29887d63cc6f41684ea9736ea5733d4d
Ahmed Abu zaid <ahmedabuzaid@Ahmeds-MacBook-Pro.local>
2019-01-16 20:23:21 +0200

    greet a new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -13,10 +13,6 @@ socket.on('disconnect', function(){
   console.log('disconnected from server');
 });
 
-// socket.on('newEmail', function(email){
-//   console.log('new email', email);
-// });
-
 socket.on('newMessage', function(message){
   console.log('new message', message);
 });
--- server/server.js
+++ server/server.js
@@ -14,34 +14,36 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
-  // socket.emit('newEmail', {
-  //   from: 'Ahmed',
-  //   text: 'what is going on?',
-  //   careatedAt: 2323
-  // });
-  //
-  // socket.on('createEmail', (newEmail) => {
-  //   console.log('createEmail', newEmail);
-  // });
+  socket.emit('newMessage', {
+    from: 'admin',
+    text: 'Hi, welcome to the chat',
+    careatedAt: new Date().getTime()
+  });
 
+  socket.broadcast.emit('newMessage',{
+    from: 'admin',
+    text: 'a new user joined',
+    careatedAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      careatedAt: new Date().getTime()
-    });
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   careatedAt: new Date().getTime()
+    // });
+    // socket.broadcast.emit('newMessage',{
+    //   from: message.from,
+    //   text: message.text,
+    //   careatedAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
     console.log('user was disconnected');
   });
 });
-
-// app.get('/me', (req, res) => {
-//   res.render('./../public/index.html');
-// });
 
 server.listen(port, () => {
   console.log(`started on port ${port}`);
