fc861a2b64a4b1c938f8bf69367171e5dc6a6815
Ahmed Abu zaid <ahmedabuzaid@Ahmeds-MacBook-Pro.local>
2019-01-16 17:39:44 +0200

    add xoconnect and disconnect events handler

--- server/server.js
+++ server/server.js
@@ -1,18 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
-var app = express();
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user was disconnected');
+  });
+});
 
 // app.get('/me', (req, res) => {
 //   res.render('./../public/index.html');
 // });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`started on port ${port}`);
 });
